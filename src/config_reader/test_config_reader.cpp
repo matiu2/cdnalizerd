e47df5d9106b6d2770025af97e925453058ad9de
Matthew Sherborne <msherborne@gmail.com>
2014-06-08 09:38:54 +1000

    Tests dequoting a plain string

--- src/config_reader/test_config_reader.cpp
+++ src/config_reader/test_config_reader.cpp
@@ -8,12 +8,17 @@ go_bandit([&](){
     using namespace cdnalizer;
 
     describe("config_reader", [&](){
-        it("1. Can read an empty config"", [&] {
+        it("1. Can read an empty config", [&] {
             std::stringstream empty;
             Config a = config_reader(empty);
             AssertThat(a.size(), Equals(0));
         };
+        it("2. Can read a username", [&] {
+            std::stringstream config;
+            config << "username=james_bond";
+            Config c = config_reader(config);
+            AssertThat(c.size(), Equals(1));
+            AssertThat(c.authDetails[0].username, Equals("james_bond"));
+        };
     };
-
-
 });
