5d0c548d47e489eb64d6db027c8bd09edebc2624
Matthew Sherborne <msherborne@gmail.com>
2014-07-14 05:20:52 +1000

    Abandoning libcurl

--- experiments/rax-login.cpp
+++ experiments/rax-login.cpp
@@ -1,18 +1,46 @@
 /// Experiment that logs in to Rackspace US API Servers
+/// ABANDONED: I'm dropping libcurl in favor of cpp-netlib
 
 //#include "curl_easy.h"
 
 #include <iostream>
 #include <stdexcept>
+#include <system_error>
+#include <sstream>
 
 #include <json.hpp>
 #include <curl.h>
+#include <sys/select.h>
 
 struct CurlErr : std::runtime_error {
   CURLcode code;
   CurlErr(CURLcode code) : std::runtime_error(curl_easy_strerror(code)), code(code) {}
 };
 
+/// A create once per app, in the main curl sentry
+struct CurlGlobalSentry {
+  CurlGlobalSentry() {
+    CURLcode result = curl_global_init(CURL_GLOBAL_SSL);
+    if (result != 0) {
+      std::stringstream msg;
+      msg << "Couldn't Initialize Curl Library. Error code: " << result;
+      throw std::runtime_error(std::move(msg.str()));
+    }
+  }
+  ~CurlGlobalSentry() {
+    curl_global_cleanup();
+  }
+};
+
+namespace {
+thread_local std::function<size_t(char* buffer, size_t size, size_t nitems, void* instream)> callback;
+extern "C" size_t read_callback(char *buffer, size_t size, size_t nitems, void *instream) {
+  return callback
+
+}
+}
+
+/// A single curl handle. One per thread please, but can be re-used for multiple requests.
 class CurlEasy {
 protected:
   CURL* handle;
@@ -20,6 +48,7 @@ public:
   CurlEasy() : handle(curl_easy_init()) {
     // Show error messages on failure
     setOpt(CURLOPT_VERBOSE, 1);
+    setOpt(CURLOPT_SSLENGINE_DEFAULT, 1);
   }
   ~CurlEasy() { curl_easy_cleanup(handle); }
   template <typename T> void setOpt(CURLoption opt, T value) const {
@@ -33,10 +62,48 @@ public:
     if (num != CURLE_OK)
       throw CurlErr(num);
   }
+  void getSocket(fd_set& result) const {
+    int socket;
+    checkError(curl_easy_getinfo(handle, CURLINFO_LASTSOCKET, &socket));
+    FD_ZERO(&result);
+    FD_SET(socket, &result);
+  }
+  void ensureWritable() {
+    /// Ensures we can send on the connection
+    fd_set writable;
+    getSocket(writable);
+    struct timeval tv{1, 0}; // Wait 1 second and 0 microsends
+    int res = select(1, nullptr, &writable, nullptr, &tv);
+    if (res < 0)
+      throw std::system_error(std::error_code(static_cast<int>(errno), std::system_category()));
+  }
+  void ensureReadable() {
+    fd_set writable;
+    getSocket(writable);
+    struct timeval tv{1, 0}; // Wait 1 second and 0 microsends
+    int res = select(1, nullptr, &writable, nullptr, &tv);
+    if (res < 0)
+      throw std::system_error(std::error_code(static_cast<int>(errno), std::system_category()));
+  }
+  void send(const std::string& data) {
+    ensureWritable();
+    size_t sent=0;
+    checkError(curl_easy_send(handle, data.c_str(), data.size(), &sent));
+    assert(sent == data.size()); // Didn't send all the data. Something's fishy.
+  }
+  std::string recv() {
+    ensureReadable();
+    size_t recieved=0;
+    checkError(curl_easy_send(handle, data.c_str(), data.size(), &received));
+    assert(received == data.size()); // Didn't send all the data. Something's fishy.
+
+
+  }
 };
 
 int main(int argc, const char *argv[]) {
   using namespace json;
+  CurlGlobalSentry curlSentry;
 
   // Get the username and api key from the command line
   if (argc != 3) {
@@ -58,8 +125,12 @@ int main(int argc, const char *argv[]) {
   CurlEasy c;
   c.setOpt(CURLOPT_URL, "https://identity.api.rackspacecloud.com/v2.0/tokens");
   c.setOpt(CURLOPT_POST, 1);
-  c.setOpt(CURLOPT_HTTPHEADER, "Content-type: application/json");
+  c.setOpt(CURLOPT_CAPATH, "/etc/ssl/certs"); // TODO: change for non-ubuntu releases
   c.setOpt(CURLOPT_CONNECT_ONLY, 1);
-  c.setOpt(CURLOPT_ERRORBUFFER, 1);
+  c.setOpt(CURLOPT_FOLLOWLOCATION, 1);
   c.perform();
+  c.send("Content-type: application/json\n\n"); // Sending our own headers. yay.
+  c.send(body.str());
+  std::string response(8192);
+
 }
