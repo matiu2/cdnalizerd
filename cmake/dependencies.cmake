568da89badf10fadc00cc18f774300152d108c45
Matthew Sherborne <msherborne@gmail.com>
2014-12-15 06:09:22 +1000

    Something weird with git happened. Trying to fix

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,69 @@
+
+include(ExternalProject)
+
+## Bandit (for tests)
+ExternalProject_Add(bandit
+    PREFIX 3rd_party
+    GIT_REPOSITORY https://github.com/joakimkarlsson/bandit.git
+    TLS_VERIFY true
+    TLS_CAINFO certs/DigiCertHighAssuranceEVRootCA.crt
+    UPDATE_COMMAND "" # Skip annoying updates for every build
+    INSTALL_COMMAND ""
+)
+SET(BANDIT_INCLUDE_DIR ${CMAKE_CURRENT_BINARY_DIR}/3rd_party/src/bandit)
+
+ExternalProject_Add(inotify_cxx
+    PREFIX 3rd_party
+    #--Download step--------------
+    URL http://inotify.aiken.cz/download/inotify-cxx/inotify-cxx-0.7.4.tar.gz
+    URL_HASH SHA1=1f009dc92c29f1b12e14212dddf8d4696c63051a
+    #--Configure step-------------
+    CONFIGURE_COMMAND ""
+    #--Build step-----------------
+    BUILD_COMMAND ""
+    #--Install step---------------
+    UPDATE_COMMAND "" # Skip annoying updates for every build
+    INSTALL_COMMAND ""
+)
+set(INOTIFY_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/3rd_party/src/inotify_cxx)
+set(INOTIFY_SOURCE_FILE ${INOTIFY_SOURCE_DIR}/inotify-cxx.cpp)
+add_custom_command(
+    OUTPUT ${INOTIFY_SOURCE_FILE}
+    DEPENDS inotify_cxx
+)
+add_library(inotify_lib STATIC ${INOTIFY_SOURCE_FILE})
+
+## jsonpp11 - JSON wrapper
+
+ExternalProject_Add(jsonpp11
+    PREFIX 3rd_party
+    GIT_REPOSITORY https://github.com/matiu2/jsonpp11.git
+    TLS_VERIFY true
+    TLS_CAINFO certs/DigiCertHighAssuranceEVRootCA.crt
+    TEST_BEFORE_INSTALL 0
+    TEST_COMMAND ctest
+    UPDATE_COMMAND "" # Skip annoying updates for every build
+    INSTALL_COMMAND ""
+)
+
+
+## curlpp11 - CURL wrapper
+
+FIND_PACKAGE(CURL REQUIRED)
+FIND_PACKAGE(OpenSSL REQUIRED)
+
+ExternalProject_Add(curlpp11
+    PREFIX 3rd_party
+    GIT_REPOSITORY https://github.com/matiu2/curlpp11.git
+    TLS_VERIFY true
+    TLS_CAINFO certs/DigiCertHighAssuranceEVRootCA.crt
+    TEST_BEFORE_INSTALL 0
+    TEST_COMMAND ctest
+    UPDATE_COMMAND "" # Skip annoying updates for every build
+    INSTALL_COMMAND ""
+)
+SET(CURLPP11_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/3rd_party/src/curlpp11/src)
+set_property(
+  TARGET curlpp11
+  APPEND
+  PROPERTY INCLUDE_DIRECTORIES ${OPENSSL_INCLUDE_DIR} ${CURL_INCLUDE_DIR})
--- experiments/rax-login.cpp
+++ experiments/rax-login.cpp
@@ -5,99 +5,14 @@
 #include <iterator>
 #include <algorithm>
 
-#include <json.hpp>
-#include <curl.h>
+#include <jsonpp11/parse_to_json_class.hpp>
+#include <curl.hpp>
 
-struct CurlErr : std::runtime_error {
-  CURLcode code;
-  CurlErr(CURLcode code) : std::runtime_error(curl_easy_strerror(code)), code(code) {}
-};
-
-struct CURLHeaders {
-  curl_slist* base=nullptr;
-  ~CURLHeaders() {
-    if (base)
-      curl_slist_free_all(base);
-  }
-  void add(const char* header) {
-    curl_slist* last = curl_slist_append(base, header);
-    if (!base)
-      base = last;
-  }
-};
-
-/// A create once per app, in the main curl sentry
-class CurlGlobalSentry {
-private:
-  static bool exists;
-public:
-  CurlGlobalSentry() {
-    assert(!exists); // There can be only one!
-    exists = true;
-    CURLcode result = curl_global_init(CURL_GLOBAL_SSL);
-    if (result != 0) {
-      std::stringstream msg;
-      msg << "Couldn't Initialize Curl Library. Error code: " << result;
-      throw std::runtime_error(std::move(msg.str()));
-    }
-  }
-  ~CurlGlobalSentry() {
-    curl_global_cleanup();
-  }
-};
-
-bool CurlGlobalSentry::exists = false;
-
-class CurlEasy {
-protected:
-  CURLHeaders headers;
-  CURL* handle;
-public:
-  CurlEasy() : handle(curl_easy_init()) {
-    // Show error messages on failure
-    setOpt(CURLOPT_VERBOSE, 0);
-  }
-  ~CurlEasy() { curl_easy_cleanup(handle); }
-  template <typename ...T> void setOpt(CURLoption opt, T... values) const {
-    checkError(curl_easy_setopt(handle, opt, values...));
-  }
-  template <typename... Types> void getInfo(CURLINFO info, Types... args) const {
-    checkError(curl_easy_getinfo(handle, info, args...));
-  }
-  void perform() const { checkError(curl_easy_perform(handle)); }
-  void checkError(CURLcode num) const {
-    if (num != CURLE_OK)
-      throw CurlErr(num);
-  }
-  void addHeader(const char* header) {
-    headers.add(header);
-    setOpt(CURLOPT_HTTPHEADER, headers.base);
-  }
-};
-
-/// Called by curl when we have data to recv
-extern "C"
-size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
-  // @userdata is a curl object
-  const size_t bytes = size * nmemb;
-  std::string* response = static_cast<std::string*>(userdata);
-  response->reserve(response->size() + bytes);
-  std::copy(ptr, ptr+bytes, std::back_inserter(*response));
-  return bytes;
-}
-
-/// Called by curl when it wants data to send
-extern "C"
-size_t read_callback(char *buffer, size_t size, size_t nitems, void *instream) {
-  const size_t bytes = size * nitems;
-  auto data = static_cast<std::stringstream *>(instream);
-  data->read(buffer, bytes);
-  return data->gcount();
-}
+using namespace json;
 
 int main(int argc, const char *argv[]) {
   using namespace json;
-  CurlGlobalSentry curl;
+  curl::GlobalSentry curl;
 
   // Get the username and api key from the command line
   if (argc != 3) {
@@ -115,22 +30,16 @@ int main(int argc, const char *argv[]) {
   std::stringstream req_body;
   req_body << j;
   std::cout << req_body.str() << std::endl;
+  std::string response_string;
 
   // Send it
-  CurlEasy c;
-  std::string response_string;
-  c.setOpt(CURLOPT_URL, "https://identity.api.rackspacecloud.com/v2.0/tokens");
-  //c.setOpt(CURLOPT_URL, "http://posttestserver.com/post.php");
-  //c.setOpt(CURLOPT_URL, "https://posttestserver.com/post.php");
-  c.setOpt(CURLOPT_POST, 1);
-  c.addHeader("Content-type: application/json");
-  c.setOpt(CURLOPT_USERAGENT , "cdnalizerd 0.1");
-  c.setOpt(CURLOPT_READFUNCTION, read_callback);
-  c.setOpt(CURLOPT_POSTFIELDSIZE, req_body.str().size());
-  c.setOpt(CURLOPT_READDATA, &req_body);
-  c.setOpt(CURLOPT_WRITEFUNCTION, write_callback);
-  c.setOpt(CURLOPT_WRITEDATA, &response_string);
-  c.perform();
-  JSON response = json::read(response_string);
+  curl::Easy c;
+    c.url("https://identity.api.rackspacecloud.com/v2.0/tokens")
+    .setHeader("Content-type: application/json")
+    .userAgent("cdnalizerd 0.1")
+    .POST()
+    .customBody(req_body)
+    .perform(response_string);
+  JSON data = readValue(response_string);
   std::cout << "Token: " << response["access"]["token"]["id"] << std::endl;
 }
