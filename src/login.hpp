450f142dc778fd07d868881c236106be455e4a12
Matthew Sherborne <msherborne@gmail.com>
2014-12-15 07:36:47 +1000

    Can now log in to rackspace

---
+++ src/login.hpp
@@ -0,0 +1,52 @@
+/// Logs in to rackspace and gives you a token
+
+#include <jsonpp11/parse_to_json_class.hpp>
+#include <curlpp11.hpp>
+
+#include <stdexcept>
+
+namespace cdnalizerd {
+
+/**
+* @brief Thrown when we can't log in to rackspace
+*/
+struct LoginFailed : std::runtime_error {
+  LoginFailed(std::string msg) : std::runtime_error(msg) {}
+};
+
+struct Rackspace {
+  void login(const std::string& username, const std::string& apikey) {
+    using namespace json;
+    // Create the request
+    JSON j(JMap{
+        {"auth", JMap{{"RAX-KSKEY:apiKeyCredentials",
+                       JMap{{"username", username}, {"apiKey", apikey}}}}}});
+    std::stringstream req_body;
+    req_body << j;
+    // Get the response
+    std::string response_string;
+    curl::Easy c;
+    c.url("https://identity.api.rackspacecloud.com/v2.0/tokens")
+        .header("Content-type: application/json")
+        .userAgent("cdnalizerd 0.1")
+        .POST()
+        .customBody(req_body)
+        .perform(response_string);
+    response = readValue(response_string.begin(), response_string.end());
+    // Make sure we got the token
+    try {
+      token = response.at("access").at("token").at("id");
+    } catch (std::out_of_range) {
+      throw LoginFailed(std::string("Couldn't get access token. Response: ") + response_string);
+    }
+    if (token.empty())
+      throw LoginFailed(std::string("Empty access token. Response: ") + response_string);
+  }
+  /// Our access token
+  std::string token;
+  /// The response from the login endpoint
+  json::JSON response;
+  /// Returns true if we are logged in and have an access token
+  operator bool() const { return !token.empty(); }
+};
+}
---
+++ src/test_login.cpp
@@ -0,0 +1,42 @@
+#include <bandit/bandit.h>
+
+#include <iostream>
+#include <sstream>
+
+#include <cstdlib>
+
+#include "login.hpp"
+
+using namespace bandit;
+using namespace cdnalizerd;
+
+std::pair<std::string, std::string> getCredentials() {
+  // Get user's home directory
+  char* username = std::getenv("OS_USERNAME");
+  char* password = std::getenv("OS_PASSWORD");
+  if ((username == nullptr) || (password == nullptr))
+    throw std::runtime_error("Unable to login, please add the environment varables. eg.\nOS_USERNAME=abc\nOS_PASSWORD=5320203405430");
+  return std::make_pair(username, password);
+}
+
+go_bandit([]() {
+
+  std::stringstream output;
+
+  before_each([&]() { output.str(""); });
+
+  describe("Login", [&]() {
+
+    it("1.1. Can login", [&]() {
+      auto creds = getCredentials();
+      Rackspace rs;
+      AssertThat(rs.token.empty(), Equals(true));
+      rs.login(creds.first, creds.second);
+      AssertThat(rs.token.empty(), Equals(false));
+    });
+
+  });
+
+});
+
+int main(int argc, char *argv[]) { return bandit::run(argc, argv); }
