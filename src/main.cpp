d60be3c6ff7edb3e1b040f5599fb2dc24da76f1a
Matthew Sherborne <msherborne@gmail.com>
2017-07-22 14:50:25 +0000

    Now you have to tell it to --go to do anything

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,8 @@ using namespace cdnalizerd;
 
 namespace po = boost::program_options;
 
-po::variables_map parseCommandline(int argc, char** argv) {
+int main(int argc, char **argv) {
+  // See if we have a --config_file option
   po::options_description desc("Allowed options");
   desc.add_options()("help", "Show help message")(
       "config", po::value<std::string>()->default_value("/etc/cdnalizerd.conf"),
@@ -23,32 +24,26 @@ po::variables_map parseCommandline(int argc, char** argv) {
       "name. Accepts files with extensions "
       ".info or .ini")("create-sample",
                        "Creates an empty sample config file at 'config' "
-                       "filename, and does nothing else");
-  po::variables_map result;
-  po::store(po::parse_command_line(argc, argv, desc), result);
-  result.notify();
-  if (result.count("help")) {
-    using namespace std;
-    cout << desc << std::endl;
-  }
-  return result;
-}
+                       "filename, and does nothing else")
+("go", "Start running...");
+  po::variables_map options;
+  po::store(po::parse_command_line(argc, argv, desc), options);
+  options.notify();
 
-int main(int argc, char **argv) {
-  // See if we have a --config_file option
-  po::variables_map options(parseCommandline(argc, argv));
+  // Handle the options
   std::string config_file_name = options["config"].as<std::string>();
   if (options.count("create-sample")) {
     cdnalizerd::write_sample_config(config_file_name);
     std::cerr << "Sample config file written to " << config_file_name << std::endl;
-  } else if (options.count("help")) {
-    // The options were already prentid in 'parseCommandline'. Just exit
-    return -1;
-  } else {
+  } else if (options.count("go")) {
     BOOST_LOG_TRIVIAL(info) << "Reading config from " << config_file_name;
     config = read_config(config_file_name);
     RESTClient::http::spawn(cdnalizerd::processes::watchForFileChanges);
     RESTClient::http::run();
     return 0;
+  } else {
+    using namespace std;
+    cout << desc << std::endl;
+    return -1;
   }
 }
