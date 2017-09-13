27bfbd65753ae243ed5dc5ac1bd9f0a74855f070
Matthew Sherborne <msherborne@gmail.com>
2017-09-14 06:02:22 +1000

    Better logging. Now I'm going to fix the URL parsing

--- src/jobs/upload.cpp
+++ src/jobs/upload.cpp
@@ -23,15 +23,19 @@ namespace jobs {
 
 /// Returns the file size and md5
 const std::string md5_from_file(const fs::path &path) {
+  LOG_SCOPE_F(5, "md5_from_file");
   unsigned char result[MD5_DIGEST_LENGTH];
   try {
     assert(fs::is_regular_file(path));
     boost::iostreams::mapped_file_source src(path.native());
     MD5((const unsigned char *)src.data(), src.size(), result);
   } catch (std::exception e) {
+    LOG_S(ERROR) << "std::exception: " << e.what();
     BOOST_THROW_EXCEPTION(boost::enable_error_info(e)
                           << err::action("Taking md5"));
   } catch (...) {
+    LOG_S(ERROR) << "Unkown exception: "
+                 << boost::current_exception_diagnostic_information(true);
     BOOST_THROW_EXCEPTION(
         boost::enable_error_info(std::runtime_error("Unknown exception"))
         << err::action("Taking md5"));
@@ -55,8 +59,9 @@ void upload(const fs::path &source, const URL &dest, HTTPS &conn,
     namespace http = boost::beast::http;
     using boost::beast::file_mode;
     if (md5.empty()) {
-      DLOG_S(5) << "Getting MD5 of local file: " << source.native();
+      DLOG_S(8) << "Getting MD5 of local file: " << source.native();
       md5 = md5_from_file(source);
+      DLOG_S(9) << "Local MD5: " << md5;
     }
     // Make the upload request
     http::request<http::file_body> req;
@@ -141,21 +146,25 @@ Job makeConditionalUploadJob(fs::path source, URL dest) {
       DLOG_S(5) << "Getting MD5 of local file: " << source.native();
       std::string md5;
       md5 = md5_from_file(source);
-      // Get the MD5 of the existing file
-      http::request<http::string_body> req{http::verb::head, dest.path, 11};
-      req.set(http::field::host, dest.host_part());
+      // Get the MD5 of the existing file from the server
+      for (char ch : dest.path)
+        std::cout << std::hex << ch;
+      DLOG_S(9) << "Path: " << dest.path << ";";
+      http::request<http::empty_body> req{http::verb::head, dest.path, 11};
+      req.set(http::field::host, dest.hostname);
       req.set(http::field::user_agent, "cdnalizerd v0.2");
       req.set(http::field::accept, "application/json");
       req.set(http::field::content_length, "0");
       req.set("X-Auth-Token", token);
-      req.method(http::verb::head);
       DLOG_S(9) << "HTTP Request: " << req;
       http::async_write(conn.stream(), req, conn.yield);
       // Get the response
-      http::response<http::empty_body> response;
+      http::response<http::string_body> response;
       http::async_read(conn.stream(), conn.read_buffer, response, conn.yield);
       DLOG_S(9) << "HTTP Response: " << response;
       if (response.result() != http::status::ok) {
+        LOG_S(ERROR) << "Bad HTTP Response. HTTP Request: " << req
+                     << "\n Response: " << response;
         BOOST_THROW_EXCEPTION(
             boost::enable_error_info(std::runtime_error("HTTP Bad Response"))
             << err::http_status(response.result()));
