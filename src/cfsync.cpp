31020dc57b469fbbd2d303f7f79969af3d513363
Matthew Sherborne <msherborne@gmail.com>
2018-02-21 23:32:35 +0000

    Made cfsync argument parsing better

--- src/cfsync.cpp
+++ src/cfsync.cpp
@@ -3,6 +3,8 @@
 #include <boost/program_options.hpp>
 
 #include <string>
+#include <iostream>
+#include <sstream>
 
 namespace po = boost::program_options;
 
@@ -11,18 +13,62 @@ struct Options {
   std::string dest;
 };
 
-
-po::options_description setupCommandlineOptions(int argc, char **argv) {
+bool setupCommandlineOptions(int argc, char **argv, Options &result) {
   po::options_description options("Options");
   options.add_options()("help", "Similar to rsync. Specify cloud files URLs "
                                 "with 'cf://' at the beginning. Everything "
                                 "else is considered a path")(
-      "source", po::value<std::string>(), "Source path or cloud file URL")(
-      "dest", po::value<std::string>(), "Destinanion path or cloud file URL");
-  return options;
+      "source", po::value<std::string>(&result.source),
+      "Source path or cloud file URL")("dest",
+                                       po::value<std::string>(&result.dest),
+                                       "Destinanion path or cloud file URL");
+  po::positional_options_description p;
+  p.add("source", 1);
+  p.add("dest", 1);
+  po::variables_map vm;
+
+  struct Error {};
+  std::stringstream error_message;
+
+  // Apply all the options if you can
+  try {
+    try {
+      po::store(po::command_line_parser(argc, argv)
+                    .options(options)
+                    .positional(p)
+                    .run(),
+                vm);
+    } catch (boost::program_options::error &e) {
+      error_message << "Error parsing options: " << e.what() << "\n";
+      throw Error();
+    }
+    po::notify(vm);
+    if (result.source.empty()) {
+      error_message << "No source file/url provided";
+      throw Error();
+    }
+    if (result.dest.empty()) {
+      error_message << "No destination file/url provided";
+      throw Error();
+    }
+    // Parse the options
+    if (vm.count("help")) {
+      options.print(std::cout, 80);
+      return false;
+    }
+  } catch (Error) {
+    std::cerr << error_message.str() << "\n";
+    options.print(std::cerr, 80);
+    return false;
+  }
+  return true;
 }
 
 int main(int argc, char **argv) {
-  po::options_description options(setupCommandlineOptions(argc, argv));
+  Options options;
+  if (!setupCommandlineOptions(argc, argv, options))
+    return 1;
+  std::cout << "Source: " << options.source << "\n"
+            << "Dest: " << options.dest << "\n";
 }
 
